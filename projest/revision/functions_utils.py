cdc4730704d931bd9c12404dc6dfb7d5892d838e
Vlad <Vladkovalov777@gmail.com>
2025-09-22 18:01:48 +0200

    1509-1

--- projest/revision/functions_utils.py
+++ projest/revision/functions_utils.py
@@ -11,3 +11,25 @@ def add_salt_to_list(given_list: list) -> None:
     print(identifier)
     logger.info("tfguyr")
     given_list.append(identifier)
+
+
+def  say_hellow_buddy():
+    return "Hellow, buddy"
+
+def return_biggest_num_from_array(arr):
+    biggest_num = arr[0]
+
+    for elem in arr:
+        if elem > biggest_num:
+            biggest_num = elem
+    logger.info(f"return_biggest_num_from_array:{arr=}")
+    return biggest_num
+arr_1 = [1,2,3,4,5,6]
+a = return_biggest_num_from_array(arr_1)
+print(a)
+
+def multiply_two_nums(one,two):
+    return one*two
+b = multiply_two_nums(one=3, two=5)
+
+print(b)
--- projest/revision/main.py
+++ projest/revision/main.py
@@ -1,4 +1,5 @@
-from revision.functions_utils import is_number_bigger_than_given, add_salt_to_list
+from revision.functions_utils import is_number_bigger_than_given, add_salt_to_list, say_hellow_buddy, \
+    return_biggest_num_from_array, multiply_two_nums
 
 
 def main():
@@ -7,6 +8,7 @@ def main():
     result = is_number_bigger_than_given(candidate_number=66, threshold=1)
     print(result)
 
+
     given_list = []
     add_salt_to_list(given_list)
     add_salt_to_list(given_list)
@@ -14,6 +16,12 @@ def main():
     add_salt_to_list(given_list)
     add_salt_to_list(given_list)
     print(given_list)
+    print(say_hellow_buddy())
+    arr_1 = [1, 2, 3, 4, 5, 6, 20]
+    print(return_biggest_num_from_array(arr_1))
+    num_one = 4
+    num_two = 6
+    print(multiply_two_nums(num_one, num_two))
 
 
 if __name__ == "__main__":
