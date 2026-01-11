0c09c7b8e1d9fe18250caf99990091093672964b
Vlad <Vladkovalov777@gmail.com>
2026-01-11 17:16:00 +0100

    Homework 9

--- projest/revision/functions_utils.py
+++ projest/revision/functions_utils.py
@@ -1,35 +1,11 @@
-from uuid import uuid4
-from service.logger import logger
-def is_number_bigger_than_given(candidate_number: float, threshold: float = 10) -> bool:
-    """according to the task #6556565"""
-    return candidate_number > threshold
+def get_ticket_price(age: int) -> float:
+    BASE_PRICE = 100.0
 
-
-def add_salt_to_list(given_list: list) -> None:
-    """WARNING: list is being modified globally"""
-    identifier = uuid4().hex
-    print(identifier)
-    logger.info("tfguyr")
-    given_list.append(identifier)
-
-
-def  say_hellow_buddy():
-    return "Hellow, buddy"
-
-def return_biggest_num_from_array(arr):
-    biggest_num = arr[0]
-
-    for elem in arr:
-        if elem > biggest_num:
-            biggest_num = elem
-    logger.info(f"return_biggest_num_from_array:{arr=}")
-    return biggest_num
-arr_1 = [1,2,3,4,5,6]
-a = return_biggest_num_from_array(arr_1)
-print(a)
-
-def multiply_two_nums(one,two):
-    return one*two
-b = multiply_two_nums(one=3, two=5)
-
-print(b)
+    if age < 6:
+        return 0.0
+    elif 6 <= age <= 17:
+        return BASE_PRICE * 0.5
+    elif 18 <= age <= 59:
+        return BASE_PRICE
+    else:
+        return BASE_PRICE * 0.7
